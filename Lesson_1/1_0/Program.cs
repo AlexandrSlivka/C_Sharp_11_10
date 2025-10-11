5a9ee11c6037d8a7b64e8ecccd5cdf9510fc4b91
Alexandr <alexandr.sliwka@yandex.ru>
2025-10-11 22:31:51 +0300

    Task 1_0

---
+++ Lesson_1/1_0/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+
+Console.WriteLine("Write a number: ");
+
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(num * num);
+
+
