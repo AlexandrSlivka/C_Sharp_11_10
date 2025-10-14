e66067df2d538e18632a10097acd12604573e985
Alexandr <alexandr.sliwka@yandex.ru>
2025-10-14 22:59:58 +0300

    Task 1_1

--- Lesson_1/1_1/Program.cs
+++ Lesson_1/1_1/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+Console.WriteLine("Введите число:");
+int num = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите число:");
+int num2 = int.Parse(Console.ReadLine()!);
+
+if (num == num2 * num2)
+  {
+    Console.WriteLine($"Число {num} являетя квадратом {num2}");
+  }
+  else
+  {
+    Console.WriteLine($"Число {num} не являетя квадратом {num2}");
+  }
