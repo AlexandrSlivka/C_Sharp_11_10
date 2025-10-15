189332dafc4a252dad2f89e4036c4afd856bf8f1
Alexandr <alexandr.sliwka@yandex.ru>
2025-10-15 14:18:36 +0300

    Task 1_3

---
+++ Lesson_1/1_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using System.Runtime.CompilerServices;
+
+Console.WriteLine("введите число");
+int number = int.Parse(Console.ReadLine()!);
+int number2 = -number;
+while (number >= number2)
+{
+  System.Console.WriteLine(number2);
+  number2++;
+}
+
+
+  
