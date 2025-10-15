448aa2af86b78f8fb395c317f34a42ff3a0e039e
Alexandr <alexandr.sliwka@yandex.ru>
2025-10-15 14:52:15 +0300

    Task 1_7

---
+++ Lesson_1/1_7/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("введите число");
+int number = int.Parse(Console.ReadLine()!);
+int number2 = 2;
+
+
+while (number2 <= number)
+{
+  System.Console.Write(number2);
+  number2 = number2 + 2;
+  
+}
