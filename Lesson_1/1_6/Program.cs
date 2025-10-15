9178c1a7cddde9331ceae463291ba9fca91fa955
Alexandr <alexandr.sliwka@yandex.ru>
2025-10-15 14:34:45 +0300

    Task 1_6

---
+++ Lesson_1/1_6/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("введите число");
+int number = int.Parse(Console.ReadLine()!);
+
+if (number % 2 == 0)
+{
+  System.Console.WriteLine("четное");
+}
+else  System.Console.WriteLine("нечетно");
