9c4bd80ea3ea5d999544c530b58dc6858a7f9437
Alexandr <alexandr.sliwka@yandex.ru>
2025-10-15 14:22:04 +0300

    Task 1_4

---
+++ Lesson_1/1_4/Program.cs
@@ -0,0 +1,11 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("введите число1");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите число2");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number > number2)
+{
+  System.Console.WriteLine(number);
+}
+else System.Console.WriteLine(number2);
