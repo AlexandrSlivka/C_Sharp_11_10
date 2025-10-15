23dd9c5277bcb9c60a97ad94fb48de78853184c3
Alexandr <alexandr.sliwka@yandex.ru>
2025-10-15 14:31:56 +0300

    Task 1_5

---
+++ Lesson_1/1_5/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("введите число1");
+int number = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите число2");
+int number2 = int.Parse(Console.ReadLine()!);
+Console.WriteLine("введите число3");
+int number3 = int.Parse(Console.ReadLine()!);
+
+if (number > number2 && number > number3)
+{
+  System.Console.WriteLine(number);
+}
+else if (number2 > number && number2 > number3)
+{
+  System.Console.WriteLine(number2);
+} 
+else if (number3 > number && number3 > number2)
+{
+  System.Console.WriteLine(number3);
+} 
