021b4861b68f74e4fd06a947b7da0e66198f28dd
Alexandr <alexandr.sliwka@yandex.ru>
2025-10-28 22:26:21 +0300

    task 2_0

--- Lesson_2/2_0/Program.cs
+++ Lesson_2/2_0/Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// Напишите программу, которая принимает на вход трехзначное чило
+// и на выход показывает последнюю цифру этого числа
+
+//функции
+
+int TakeNum(int num)
+{
+  System.Console.WriteLine(num);
+  return num % 10;
+}
+
+int th_num = new Random().Next(100, 1000);//убирает 1 от 1000, будет до 999
+int result = TakeNum(th_num);
+System.Console.WriteLine(result);
