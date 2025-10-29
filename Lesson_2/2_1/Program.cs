9b5101787b7005c4ba02792355182a5e7b9409fc
Alexandr <alexandr.sliwka@yandex.ru>
2025-10-29 22:40:23 +0300

    task 2_1

---
+++ Lesson_2/2_1/Program.cs
@@ -0,0 +1,14 @@
+﻿int LargestNumber(int num)
+{
+  System.Console.WriteLine(num);
+  int num1 = num / 10;
+  int num2 = num % 10;
+  if (num1 > num2)
+    return num1;
+  return num2;
+}
+
+int largest = new Random().Next(10, 100);//убирает 1 от 1000, будет до 999
+int result = LargestNumber(largest);
+//int largest = result(new Random().Next(10, 100));
+System.Console.WriteLine(result);
