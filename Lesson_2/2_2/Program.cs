5d0630ed2e6ae18b355946a8aef982d4725b1e2d
Alexandr <alexandr.sliwka@yandex.ru>
2025-10-29 22:59:50 +0300

    task 2_2

---
+++ Lesson_2/2_2/Program.cs
@@ -0,0 +1,19 @@
+﻿
+// кратность числа. кратно ли первое число второму 
+int number, number1;
+System.Console.Write("Введите число 1: ");
+number = int.Parse(Console.ReadLine());
+
+System.Console.Write("Введите число 2: ");
+number1 = int.Parse(Console.ReadLine());
+
+void multiplicityNumber(int num, int num1)
+{
+  if (number % number1 == 0)
+    System.Console.WriteLine($"Число {number} кратно числу {number1}");
+  else
+  {
+    System.Console.WriteLine(number % number1);
+  }
+}
+multiplicityNumber(number, number1);
