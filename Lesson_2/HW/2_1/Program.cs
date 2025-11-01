8249b08f4744e912790524b0911254176397fa13
Alexandr <alexandr.sliwka@yandex.ru>
2025-11-01 22:45:03 +0300

    HW 2_1

---
+++ Lesson_2/HW/2_1/Program.cs
@@ -0,0 +1,17 @@
+﻿//Напишите программу, которая принимает на вход 
+//трёхзначное число и на выходе показывает вторую цифру 
+//этого числа.
+
+int th_number;
+System.Console.WriteLine("Введите трехзначное число: ");
+th_number = int.Parse(Console.ReadLine()!);
+
+int threeDigitNumber(int number)
+{
+  
+  th_number /= 10;
+  th_number %= 10;
+  return th_number;
+}
+threeDigitNumber(th_number);
+System.Console.WriteLine(th_number);
