d3ef6e9aa9399b1de54a64d823d59e64dcae55b2
Alexandr <alexandr.sliwka@yandex.ru>
2025-11-02 22:56:02 +0300

    HW 2_4

---
+++ Lesson_2/HW/2_4/Program.cs
@@ -0,0 +1,17 @@
+﻿// Написать программу, которая выводит случайное трехзначное 
+//число и удаляет вторую цифру из него
+
+int th_number;
+th_number = new Random().Next(100, 1000);
+
+int randomThreeDigitNumber(int number)
+{
+  System.Console.WriteLine(th_number);
+  int thirdNumber = th_number % 10;
+  int firstNumber = (th_number / 100) * 10;
+  th_number = thirdNumber + firstNumber;
+  return th_number;
+  
+}
+randomThreeDigitNumber(th_number);
+System.Console.WriteLine(th_number);
