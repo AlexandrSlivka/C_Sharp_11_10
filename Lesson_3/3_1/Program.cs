23458ea3dd05825c757787e7b041e134e746302b
Alexandr <alexandr.sliwka@yandex.ru>
2025-11-04 22:47:39 +0300

    HW 3_1,3_2

---
+++ Lesson_3/3_1/Program.cs
@@ -0,0 +1,17 @@
+﻿
+
+void Cordinate(int a)
+{
+  if (a == 1)
+    System.Console.WriteLine("x > 0 and y > 0");
+  else if (a == 2)
+    System.Console.WriteLine("x < 0 and y > 0");
+  else if (a == 3)
+    System.Console.WriteLine("x < 0 and y < 0");
+  else if (a == 4)
+    System.Console.WriteLine("x > 0 and y < 0");
+else System.Console.WriteLine("Нет такой четверти");
+}
+int quarter = int.Parse(Console.ReadLine()!);
+Cordinate(quarter);
+
---
+++ Lesson_3/3_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу, которая на вход принимает координаты
+// двух точек и находит расстояние между ними в 2d простаранстве
+
+double Distance(int x1, int y1, int x2, int y2)
+{
+  return Math.Sqrt(Math.Pow(x2 - x1, 2) + Math.Pow(y2 - y1, 2));
+}
+int x_1 = int.Parse(Console.ReadLine()!);
+int y_1 = int.Parse(Console.ReadLine()!);
+int x_2 = int.Parse(Console.ReadLine()!);
+int y_2 = int.Parse(Console.ReadLine()!);
+
+double result = Distance(x_1, y_1, x_2, y_2);
+System.Console.WriteLine(result);
+
+
