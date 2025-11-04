2dc5e00c1e35ab09f6d56bff4568e81dd18c520d
Alexandr <alexandr.sliwka@yandex.ru>
2025-11-04 14:55:06 +0300

    Task 3_0

---
+++ Lesson_3/3_0/Program.cs
@@ -0,0 +1,19 @@
+﻿
+
+void Quarters(int x, int y)
+{
+  if (x == 0 || y == 0)
+    System.Console.WriteLine("x=o, y =o");
+  else if (x > 0 && y > 0)
+    System.Console.WriteLine("I");
+  else if (x < 0 && y > 0)
+    System.Console.WriteLine("II");
+  else if (x < 0 && y < 0)
+    System.Console.WriteLine("III");
+  else if (x > 0 && y < 0)
+    System.Console.WriteLine("IV");
+
+}
+int num_1 = int.Parse(Console.ReadLine()!);
+int num_2 = int.Parse(Console.ReadLine()!);
+Quarters(num_1, num_2);
