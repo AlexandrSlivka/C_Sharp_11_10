9256e2107d85cbf3ae9d0971fffdae6a6ce34b0e
Alexandr <alexandr.sliwka@yandex.ru>
2025-11-05 14:51:11 +0300

    Task 3_3

---
+++ Lesson_3/3_3/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите программу, которая принимает на вход число N 
+// и выдает таблицу квадратов числе от 1 до N
+
+using System.ComponentModel;
+
+void SquareNumbers(int number)
+{
+  double count = 1;
+  double volue;
+  while (count <= number)
+  {
+
+    volue = Math.Pow(count, 2);
+    System.Console.Write($"{volue} ");
+    count++;
+    
+    
+  }
+  
+}
+int num = int.Parse(Console.ReadLine()!);
+SquareNumbers(num);
