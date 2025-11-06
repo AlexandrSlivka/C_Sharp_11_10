3a92dc859b4fd55f49bb788ec80875105059a0c2
Alexandr <alexandr.sliwka@yandex.ru>
2025-11-06 14:56:27 +0300

    Task 4_1

---
+++ Lesson_4/4_1/Program.cs
@@ -0,0 +1,18 @@
+﻿// Напишите программу, которая принимает число и показывает
+// сколько в нем цифр
+
+int NumberDigits(int number)
+{
+  int count = 0;
+  for (int i = 1; number > 0; i++)
+  {
+    number /= 10;
+    count = i;
+  }
+  return count;
+}
+System.Console.WriteLine("Введите число: ");
+int value = int.Parse(Console.ReadLine()!);
+int result = NumberDigits(value);
+System.Console.WriteLine(result);
+
