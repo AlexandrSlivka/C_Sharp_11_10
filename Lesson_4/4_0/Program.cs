255188955b57980ff96de9ee3d50bc7f14aaaf5d
Alexandr <alexandr.sliwka@yandex.ru>
2025-11-06 14:40:55 +0300

    Task 4_0

---
+++ Lesson_4/4_0/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу, ко тоторая принимает на вход число А
+// и выдает сумму чисел от 1 до А
+
+int SumNum(int num)
+{
+  int all_sum = 0;
+
+  for (int i = 0; i <= num; i++)
+    all_sum += i;
+
+  return all_sum;
+}
+
+int val = int.Parse(Console.ReadLine()!);
+int result = SumNum(val);
+System.Console.WriteLine(result);
