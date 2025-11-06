3528ce2ad1b7dc365343701f2727982e4a399d46
Alexandr <alexandr.sliwka@yandex.ru>
2025-11-06 15:22:19 +0300

    Task 4_2

---
+++ Lesson_4/4_2/Program.cs
@@ -0,0 +1,16 @@
+﻿// Написать программу, которая принимает число N и выдает
+// произведение числе от 1 до N
+
+int ProductNumbers(int number)
+{
+  int count = 1;
+  for (int i = 1; i <= number; i++)
+  {
+    count *= i;
+  }
+  return count;
+}
+System.Console.WriteLine("Введите число: ");
+int value = int.Parse(Console.ReadLine()!);
+int result = ProductNumbers(value);
+System.Console.WriteLine(result);
