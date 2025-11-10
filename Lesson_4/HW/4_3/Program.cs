c59d738efd06054befa1bb17fb3b2b64ce879a20
Alexandr <alexandr.sliwka@yandex.ru>
2025-11-10 15:42:11 +0300

    HW 4_3

---
+++ Lesson_4/HW/4_3/Program.cs
@@ -0,0 +1,35 @@
+﻿//Напишите программу, которая задаёт массив из 8 
+//элементов и выводит их на экран.
+
+int[] SettingArray(int firstNumber, int secondNumber, int size)
+{
+  int[] myArray = new int[size];
+
+  for (int i = 0; i < size; i++)
+      myArray[i] = new Random().Next(firstNumber, secondNumber + 1);
+  return myArray;
+}
+
+void ArrayOutput(int[] arr)
+{
+  int size = arr.Length;
+
+  System.Console.WriteLine("Вывод массива: ");
+  for (int i = 0; i < size; i++)
+  {
+    System.Console.Write($" {arr[i]} ");
+  }
+  System.Console.WriteLine();
+}
+
+System.Console.Write("Ведите количество элементов в массиве: ");
+int numberElementsArray = int.Parse(Console.ReadLine()!);
+
+System.Console.Write("Ведите начало диапазона массива: ");
+int beginningRange = int.Parse(Console.ReadLine()!);
+
+System.Console.Write("Ведите конец диапазона массива: ");
+int endRange = int.Parse(Console.ReadLine()!);
+int[] mass = SettingArray(beginningRange, endRange, numberElementsArray);
+
+ArrayOutput(mass);
