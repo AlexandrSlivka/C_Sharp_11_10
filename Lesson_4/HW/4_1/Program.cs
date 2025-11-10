b35b10028b6871277f16d56ce1c587b97ef0fe9c
Alexandr <alexandr.sliwka@yandex.ru>
2025-11-10 14:26:24 +0300

    HW 4_1 4_2

---
+++ Lesson_4/HW/4_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Напишите цикл, который принимает на вход два 
+//числа (A и B) и возводит число A в натуральную степень B.
+
+
+int Exponentiation(int number, int number2)
+{
+  int multiplication = 1;
+  for (int i = 1; i <= number2; i++)
+  {
+
+    multiplication *= number;
+
+  }
+  return multiplication;
+}
+System.Console.Write("Введите число А: ");
+int valueFirst = int.Parse(Console.ReadLine()!);
+System.Console.Write("Введите число В: ");
+int valueSecond = int.Parse(Console.ReadLine()!);
+int result = Exponentiation(valueFirst, valueSecond);
+
+System.Console.WriteLine($"Число {valueFirst} в степени {valueSecond} = {result}");
---
+++ Lesson_4/HW/4_2/Program.cs
@@ -0,0 +1,27 @@
+﻿// : Напишите программу, которая принимает на вход число 
+//и выдаёт сумму цифр в числе.
+
+using System.Runtime.InteropServices;
+
+
+
+int SumNumber(int value)
+{
+  int count = 0;
+  int sum = 0;
+
+  while (value > 0)
+  {
+    count = value % 10;
+
+    value /= 10;
+    sum += count;
+
+  }
+  
+  return sum;
+}
+
+int val = int.Parse(Console.ReadLine()!);
+int result = SumNumber(val);
+System.Console.WriteLine(result);
